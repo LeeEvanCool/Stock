a80128ae486693233f2549442e2415d160a06bc0
lixiaoyan <lixiaoyan@zai360.com>
2016-08-26 14:49:32 +0800

    线程版
    线程版

--- stockManager/src/pers/lxy/main/StockMain.java
+++ stockManager/src/pers/lxy/main/StockMain.java
@@ -13,10 +13,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import pers.lxy.database.DBManager;
+import pers.lxy.test.StockThread;
 
 public class StockMain {
 	
@@ -28,58 +27,51 @@ public class StockMain {
 	public static final String STOCKCODETABLE = "xx_stock_code_table_tmp";
 	
 	public static void main(String[] args) {
-		if(args == null || args.length <= 0){
+		if (args == null || args.length <= 0) {
 			System.out.println("方法带参数：1、实时数据。2、当天数据");
 			return;
 		}
 		String str = args[0];
-		if(str == null || str.isEmpty()){
+		if (str == null || str.isEmpty()) {
 			System.out.println("参数不正确");
 			return;
 		}
 		final List<Map<String, String>> stack = getStackValue();
-		if(str.equals("1")){
-			Timer timer = new Timer();
-			timer.schedule(new TimerTask() {
-				@Override
-				public void run() {
-					try {
-						getStockDate(stack);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}, 1000, 5000);
-		}else if(str.equals("2")){
+		if (str.equals("1")) {
+			for (Map<String, String> map : stack) {
+				StockThread stockThread = new StockThread(map.get("stockCode"), map.get("stockTable"));
+				Thread thread = new Thread(stockThread);
+				thread.start();
+			}
+		} else if (str.equals("2")) {
 			getStockDayDate(stack);
-		}else{
+		} else {
 			System.out.println("参数不正确");
 		}
 	}
+	
 	/**
 	 * 获取股票信息
 	 * 
 	 * @return List<Map<String,String>>
 	 */
-	private static List<Map<String, String>> getStackValue(){
+	private static List<Map<String, String>> getStackValue() {
 		Connection conn = null;
 		PreparedStatement prepareStatement = null;
 		ResultSet resultSet = null;
 		List<Map<String, String>> result = new ArrayList<Map<String, String>>();
 		try {
 			conn = DBManager.getConnection();
-			String sql = "select stock_code, stock_name, stock_table from " + STOCKCODETABLE + " where is_enable = '1' ";
+			String sql = "select stock_code, stock_table from " + STOCKCODETABLE + " where is_enable = '1' ";
 			prepareStatement = conn.prepareStatement(sql);
 			resultSet = prepareStatement.executeQuery();
-			if(resultSet != null){
-				while(resultSet.next()){
+			if (resultSet != null) {
+				while (resultSet.next()) {
 					String stockCode = resultSet.getString("stock_code");
-					String stockName = resultSet.getString("stock_name");
 					String stockTable = resultSet.getString("stock_table");
-					if(stockCode != null && stockName != null && stockTable != null){
+					if (stockCode != null && stockTable != null) {
 						Map<String, String> map = new HashMap<String, String>();
 						map.put("stockCode", stockCode);
-						map.put("stockName", stockName);
 						map.put("stockTable", stockTable);
 						result.add(map);
 					}
@@ -108,6 +100,7 @@ public class StockMain {
 		}
 		return null;
 	}
+	
 	private static void getStockDayDate(List<Map<String, String>> stack) {
 		if (stack != null && stack.size() > 0) {
 			HttpURLConnection conn = null;
@@ -150,93 +143,9 @@ public class StockMain {
 			}
 		}
 	}
-	private static void getStockDate(List<Map<String, String>> stack) {
-		if (stack != null && stack.size() > 0) {
-			HttpURLConnection conn = null;
-			BufferedReader reader = null;
-			for (Map<String, String> stockMap : stack) {
-				String url = GETURL + stockMap.get("stockCode");
-				try {
-					URL getUrl = new URL(url);
-					conn = (HttpURLConnection) getUrl.openConnection();
-					conn.connect();
-					reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "gbk"));
-					String lines;
-					StringBuffer result = new StringBuffer();
-					while ((lines = reader.readLine()) != null) {
-						result.append(lines);
-					}
-					int startIndex = result.indexOf("\"") + 1;
-					int endIndex = result.lastIndexOf("\"");
-					String str = result.substring(startIndex, endIndex);
-					System.out.println(str);
-					storeStockData(str, stockMap.get("stockTable"));
-					reader.close();
-					conn.disconnect();
-				} catch (Exception e) {
-					e.printStackTrace();
-				} finally {
-					try {
-						if (reader != null) {
-							reader.close();
-							reader = null;
-						}
-						if (conn != null) {
-							conn.disconnect();
-							conn = null;
-						}
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-	}
 	
-	private static void storeStockData(String result, String stockTable){
+	private static void storeStockDayData(String result, String stockCode) {
 		if (result != null && !result.isEmpty()) {
-			String[] split = result.split(",");
-			String stock_value_date = split[30];
-			String stock_value_time = split[31];
-			double stock_value = Double.parseDouble(split[3]);
-			Connection conn = null;
-			PreparedStatement prepareStatement = null;
-			try {
-				conn = DBManager.getConnection();
-				String sql = "insert into "
-						+ stockTable
-						+ " (create_datetime, stock_value_date, stock_value_time, stock_value) values (?, ?, ?, ?)";
-				prepareStatement = conn.prepareStatement(sql);
-				
-				java.util.Date utilDate = new java.util.Date();
-				java.sql.Timestamp sqlDate = new java.sql.Timestamp(utilDate.getTime());
-				prepareStatement.setTimestamp(1, sqlDate);
-				prepareStatement.setString(2, stock_value_date);
-				prepareStatement.setString(3, stock_value_time);
-				prepareStatement.setDouble(4, stock_value);
-				
-				prepareStatement.execute();
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if (prepareStatement != null) {
-						prepareStatement.close();
-						prepareStatement = null;
-					}
-					if (conn != null) {
-						conn.close();
-						conn = null;
-					}
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-	private static void storeStockDayData(String result, String stockCode){
-		if (result != null && !result.isEmpty()) {
-			
 			String[] split = result.split(",");
 			String stock_value_date = split[30];
 			String stock_value_time = split[31];
--- stockManager/src/pers/lxy/test/StockThread.java
+++ stockManager/src/pers/lxy/test/StockThread.java
@@ -14,12 +14,14 @@ import java.util.TimerTask;
 import pers.lxy.database.DBManager;
 
 /**
- * <p>TODO 类描述</p>
+ * <p>
+ * TODO 线程
+ * </p>
  *
- * @author  lxy
- * @date    2016年8月26日
+ * @author lxy
+ * @date 2016年8月26日
  */
-public class StockThread implements Runnable{
+public class StockThread implements Runnable {
 	
 	public static final String GETURL = "http://hq.sinajs.cn/list=";
 	
@@ -27,10 +29,11 @@ public class StockThread implements Runnable{
 	
 	private String stockTable;
 	
-	public StockThread(){
+	public StockThread() {
 		
 	}
-	public StockThread(String stockCode, String stockTable){
+	
+	public StockThread(String stockCode, String stockTable) {
 		this.stockCode = stockCode;
 		this.stockTable = stockTable;
 	}
@@ -49,6 +52,7 @@ public class StockThread implements Runnable{
 			}
 		}, 1000, 5000);
 	}
+	
 	private static void getStockDate(String stockCode, String stockTable) {
 		if (stockCode != null && !stockCode.isEmpty() && stockTable != null && !stockTable.isEmpty()) {
 			HttpURLConnection conn = null;
@@ -87,10 +91,10 @@ public class StockThread implements Runnable{
 					e.printStackTrace();
 				}
 			}
-		
 		}
 	}
-	private static void storeStockData(String result, String stockTable){
+	
+	private static void storeStockData(String result, String stockTable) {
 		if (result != null && !result.isEmpty()) {
 			String[] split = result.split(",");
 			String stock_value_date = split[30];
@@ -100,8 +104,7 @@ public class StockThread implements Runnable{
 			PreparedStatement prepareStatement = null;
 			try {
 				conn = DBManager.getConnection();
-				String sql = "insert into "
-						+ stockTable
+				String sql = "insert into " + stockTable
 						+ " (create_datetime, stock_value_date, stock_value_time, stock_value) values (?, ?, ?, ?)";
 				prepareStatement = conn.prepareStatement(sql);
 				
